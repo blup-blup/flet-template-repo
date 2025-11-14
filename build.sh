3bb214d02e622613047b5866805d15a53afd2ea5
ariefminardi <ariefminardi005@gmail.com>
2025-11-14 11:49:24 +0700

    add information

---
+++ build.sh
@@ -0,0 +1,4 @@
+uv run flet build \
+  --android-permissions android.permission.READ_EXTERNAL_STORAGE=True \
+                        android.permission.CAMERA=True \
+  apk
---
+++ test.py
@@ -0,0 +1,10 @@
+# import subprocess
+
+# input_file = "video.mp4"
+# output_file = "output.gif"
+
+# subprocess.run([
+#     "ffmpeg", "-t", "5", "-i", input_file,
+#     "-vf", "fps=20,scale=320:-1:flags=lanczos",
+#     "-loop", "0", output_file
+# ])
